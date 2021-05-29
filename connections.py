a42ee45cd9f702a3f3c45a79a9826c938a2ca317
Niels Poulsen <nielsjenspoulsen@gmail.com>
2021-05-29 15:06:13 +0200

    made TripSegment compliant with other methods

--- connections.py
+++ connections.py
@@ -24,22 +24,19 @@ class Connection(object):
 class TripSegment(object):
 
     def __init__(self, enter_connection: Connection, exit_connection: Connection):
-        self.id = enter_connection.trip_id
+        self.trip_id = enter_connection.trip_id
         self.transport_type = enter_connection.transport_type
         self.enter_connection = enter_connection
         self.exit_connection = exit_connection
 
     def __repr__(self):
-        return f'<TripSegment ({self.id}, {self.enter_connection.dep_stop} -> {self.exit_connection.arr_stop})>'
+        return f'<TripSegment ({self.trip_id}, {self.enter_connection.dep_stop} -> {self.exit_connection.arr_stop})>'
 
     def __str__(self):
-        s = f'TripSegment of trip {self.id}'
+        s = f'TripSegment of trip {self.trip_id}'
         s += f'  from {self.enter_connection.dep_stop} to {self.exit_connection.arr_stop}'
         s += f'  departure {self.enter_connection.dep_time}, arrival {self.exit_connection.arr_time}'
         return s
-
-    def trip_id(self):
-        return self.id
 
     def entry_time(self) -> datetime:
         return self.enter_connection.dep_time
